2fc3ffb57b913970fb16d5e190215ce2bd5ca16b
Evangelos Tsagkournis <Vangelis.Tsagkournis@outlook.com>
2022-05-01 07:14:16 +0300

    made helper functions methods of the class

--- detect_poi/src/calculate_aruco_distance.cpp
+++ detect_poi/src/calculate_aruco_distance.cpp
@@ -22,114 +22,11 @@
 // Node template
 #include "node_template.cpp"
 
-
-/////////////////////////////////////////////////////////////////////////////
-// Helper material to find whether a point is inside the area of an object //
-/////////////////////////////////////////////////////////////////////////////
+//static const std::string OPENCV_WINDOW = "Image Window";
 
 // Define Infinite (Using INT_MAX caused overflow problems) 
 #define INF 10000
 
-struct Point 
-{ 
-    int x; 
-    int y; 
-}; 
-
-// Given three collinear points p, q, r, the function checks if 
-// point q lies on line segment 'pr' 
-bool onSegment(Point p, Point q, Point r) 
-{ 
-  if (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) && q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y))
-    return true; 
-  return false; 
-} 
- 
-// To find orientation of ordered triplet (p, q, r). 
-// The function returns following values 
-// 0 --> p, q and r are collinear 
-// 1 --> Clockwise 
-// 2 --> Counterclockwise 
-int orientation(Point p, Point q, Point r) 
-{ 
-    int val = (q.y - p.y) * (r.x - q.x) - 
-            (q.x - p.x) * (r.y - q.y); 
- 
-    if (val == 0) return 0; // collinear 
-    return (val > 0)? 1: 2; // clock or counterclock wise 
-} 
- 
-// The function that returns true if line segment 'p1q1' 
-// and 'p2q2' intersect. 
-bool doIntersect(Point p1, Point q1, Point p2, Point q2) 
-{ 
-    // Find the four orientations needed for general and 
-    // special cases 
-    int o1 = orientation(p1, q1, p2); 
-    int o2 = orientation(p1, q1, q2); 
-    int o3 = orientation(p2, q2, p1); 
-    int o4 = orientation(p2, q2, q1); 
- 
-    // General case 
-    if (o1 != o2 && o3 != o4) 
-        return true; 
- 
-    // Special Cases 
-    // p1, q1 and p2 are collinear and p2 lies on segment p1q1 
-    if (o1 == 0 && onSegment(p1, p2, q1)) return true; 
- 
-    // p1, q1 and p2 are collinear and q2 lies on segment p1q1 
-    if (o2 == 0 && onSegment(p1, q2, q1)) return true; 
- 
-    // p2, q2 and p1 are collinear and p1 lies on segment p2q2 
-    if (o3 == 0 && onSegment(p2, p1, q2)) return true; 
- 
-    // p2, q2 and q1 are collinear and q1 lies on segment p2q2 
-    if (o4 == 0 && onSegment(p2, q1, q2)) return true; 
- 
-    return false; // Doesn't fall in any of the above cases 
-} 
- 
-// Returns true if the point p lies inside the polygon[] with n vertices 
-bool isInside(Point polygon[], Point p) 
-{ 
-    int n = sizeof(polygon) / sizeof(polygon[0]);
-    // There must be at least 3 vertices in polygon[] 
-    if (n < 3) return false; 
- 
-    // Create a point for line segment from p to infinite 
-    Point extreme = {INF, p.y}; 
- 
-    // Count intersections of the above line with sides of polygon 
-    int count = 0, i = 0; 
-    do
-    { 
-        int next = (i+1)%n; 
- 
-        // Check if the line segment from 'p' to 'extreme' intersects 
-        // with the line segment from 'polygon[i]' to 'polygon[next]' 
-        if (doIntersect(polygon[i], polygon[next], p, extreme)) 
-        { 
-            // If the point 'p' is collinear with line segment 'i-next', 
-            // then check if it lies on segment. If it lies, return true, 
-            // otherwise false 
-            if (orientation(polygon[i], p, polygon[next]) == 0) 
-            return onSegment(polygon[i], p, polygon[next]); 
- 
-            count++; 
-        } 
-        i = next; 
-    } while (i != 0); 
- 
-    // Return true if count is odd, false otherwise 
-    return count&1; // Same as (count%2 == 1) 
-} 
-
-////////////////////////////////////////////////////////////////////////////////////
-// End of helper material to find whether a point is inside the area of an object //
-////////////////////////////////////////////////////////////////////////////////////
-
-//static const std::string OPENCV_WINDOW = "Image Window";
 
 class CalculateArucoDistance
 {
@@ -149,6 +46,11 @@ private:
   } U_FloatConvert;
 
   ImageSubscriber depth_image_sub_;
+
+  bool onSegment(cv::Point2i, cv::Point2i, cv::Point2i);
+  int orientation(cv::Point2i, cv::Point2i, cv::Point2i);
+  bool doIntersect(cv::Point2i, cv::Point2i, cv::Point2i, cv::Point2i);
+  bool isInside(cv::Point2i[], cv::Point2i);
 
   int ReadDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::ImageConstPtr depth_image);
 
@@ -188,6 +90,95 @@ CalculateArucoDistance::CalculateArucoDistance(ros::NodeHandle nh, ros::NodeHand
 
 CalculateArucoDistance::~CalculateArucoDistance() 
 {
+}
+
+// Given three collinear points p, q, r, the function checks if 
+// point q lies on line segment 'pr' 
+bool CalculateArucoDistance::onSegment(cv::Point2i p, cv::Point2i q, cv::Point2i r) 
+{ 
+  if (q.x <= std::max(p.x, r.x) && q.x >= std::min(p.x, r.x) && q.y <= std::max(p.y, r.y) && q.y >= std::min(p.y, r.y))
+    return true; 
+  return false; 
+} 
+ 
+// To find orientation of ordered triplet (p, q, r). 
+// The function returns following values 
+// 0 --> p, q and r are collinear 
+// 1 --> Clockwise 
+// 2 --> Counterclockwise 
+int CalculateArucoDistance::orientation(cv::Point2i p, cv::Point2i q, cv::Point2i r) 
+{ 
+    int val = (q.y - p.y) * (r.x - q.x) - 
+            (q.x - p.x) * (r.y - q.y); 
+ 
+    if (val == 0) return 0; // collinear 
+    return (val > 0)? 1: 2; // clock or counterclock wise 
+} 
+ 
+// The function that returns true if line segment 'p1q1' 
+// and 'p2q2' intersect. 
+bool CalculateArucoDistance::doIntersect(cv::Point2i p1, cv::Point2i q1, cv::Point2i p2, cv::Point2i q2) 
+{ 
+    // Find the four orientations needed for general and 
+    // special cases 
+    int o1 = orientation(p1, q1, p2); 
+    int o2 = orientation(p1, q1, q2); 
+    int o3 = orientation(p2, q2, p1); 
+    int o4 = orientation(p2, q2, q1); 
+ 
+    // General case 
+    if (o1 != o2 && o3 != o4) 
+        return true; 
+ 
+    // Special Cases 
+    // p1, q1 and p2 are collinear and p2 lies on segment p1q1 
+    if (o1 == 0 && onSegment(p1, p2, q1)) return true; 
+ 
+    // p1, q1 and p2 are collinear and q2 lies on segment p1q1 
+    if (o2 == 0 && onSegment(p1, q2, q1)) return true; 
+ 
+    // p2, q2 and p1 are collinear and p1 lies on segment p2q2 
+    if (o3 == 0 && onSegment(p2, p1, q2)) return true; 
+ 
+    // p2, q2 and q1 are collinear and q1 lies on segment p2q2 
+    if (o4 == 0 && onSegment(p2, q1, q2)) return true; 
+ 
+    return false; // Doesn't fall in any of the above cases 
+} 
+ 
+// Returns true if the point p lies inside the polygon[] with n vertices 
+bool CalculateArucoDistance::isInside(cv::Point2i polygon[], cv::Point2i p) 
+{ 
+    int n = sizeof(polygon) / sizeof(polygon[0]);
+    // There must be at least 3 vertices in polygon[] 
+    if (n < 3) return false; 
+ 
+    // Create a point for line segment from p to infinite 
+    cv::Point2i extreme = {INF, p.y}; 
+ 
+    // Count intersections of the above line with sides of polygon 
+    int count = 0, i = 0; 
+    do
+    { 
+        int next = (i+1)%n; 
+ 
+        // Check if the line segment from 'p' to 'extreme' intersects 
+        // with the line segment from 'polygon[i]' to 'polygon[next]' 
+        if (doIntersect(polygon[i], polygon[next], p, extreme)) 
+        { 
+            // If the point 'p' is collinear with line segment 'i-next', 
+            // then check if it lies on segment. If it lies, return true, 
+            // otherwise false 
+            if (orientation(polygon[i], p, polygon[next]) == 0) 
+            return onSegment(polygon[i], p, polygon[next]); 
+ 
+            count++; 
+        } 
+        i = next; 
+    } while (i != 0); 
+ 
+    // Return true if count is odd, false otherwise 
+    return count&1; // Same as (count%2 == 1) 
 }
 
 int CalculateArucoDistance::ReadDepthData(unsigned int height_pos, unsigned int width_pos, sensor_msgs::ImageConstPtr depth_image){
@@ -276,7 +267,7 @@ void CalculateArucoDistance::callBack(
     operator_intent_msgs::pixel_coordinates_with_distance pixel_coordinates_with_distance;
     double sum = 0;
     unsigned long count = 0;
-    Point marker[4];
+    cv::Point2i marker[4];
     for (unsigned long int j = 0; j < /*4*/marker_collection->markers[i].corner_points.size(); j++){
       marker[j].x = marker_collection->markers[i].corner_points[j].x;
       marker[j].y = marker_collection->markers[i].corner_points[j].y;
@@ -286,7 +277,7 @@ void CalculateArucoDistance::callBack(
     for (int j = 0; j < (sizeof(marker)/sizeof(marker[0])); j++){
       marker_points[j] = cv::Point2i(marker[j].x, marker[j].y);
     }
-    Point intersection_point;
+    cv::Point2i intersection_point;
     if (intersection(marker_points[0], marker_points[2], marker_points[1], marker_points[3], intersection_point_cv)){
       pixel_coordinates_with_distance.distance = ReadDepthData((unsigned int)intersection_point_cv.y, (unsigned int) intersection_point_cv.x, image);
       std::cout 
