02e15b21e20866bcfcd335f89184528e1ea280ce
Evangelos Tsagkournis <Vangelis.Tsagkournis@outlook.com>
2022-11-01 17:30:58 +0200

    modified:   detect_poi/include/node_template.h

--- detect_poi/include/node_template.h
+++ detect_poi/include/node_template.h
@@ -1,5 +1,9 @@
-// MARKED AS UNUSED
+#ifndef NODE_TEMPLATE_H
+#define NODE_TEMPLATE_H
+
 #include <ros/ros.h>
 
 template <typename T>
 int NodeMain(int argc, char **argv, std::string const &nodeName);
+
+#endif // NODE_TEMPLATE_H
